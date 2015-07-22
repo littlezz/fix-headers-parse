1d4bbadf4c87a66db3ba0cf7a5d01b2aea8f7619
zz <zz.at.field@gmail.com>
2015-07-22 15:10:06 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+__author__ = 'zz'
+
+from setuptools import setup
+
+setup(
+    name='fix_headers_parse',
+    version=0.1,
+    author='zz',
+    author_email='zz.at.field@gmail.com',
+    url='https://github.com/littlezz/fix-headers-parse',
+    description='monkey patch to fix python3 headers parse when headers contain some special character, eg, some chinese',
+    packages=['fix_headers_parse'],
+    license='MIT',
+)
+
